08a5de45944edc474ad99f987c070a48b9ce2839
kwon <dohun_dev@naver.com>
2024-02-14 19:51:59 +0900

    Board Project first commit(changing repository)

---
+++ pages/boards/[boardId]/index.js
@@ -0,0 +1,7 @@
+import DetailPage from '../../../src/components/units/board/detail/BoardDetail.container'
+export default function BoardDetailPage() {
+
+  return (
+    <DetailPage />
+  );
+}
---
+++ pages/boards/new/index.js
@@ -0,0 +1,6 @@
+import BoardDetailContainer from "../../../src/components/units/board/write/BoardWrite.container";
+export default function BoardsNewPage() {
+  return (
+    <BoardDetailContainer />
+  );
+}
